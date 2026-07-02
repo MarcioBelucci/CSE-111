203ff81931389ed3c55a66d74aeddfc1698b3118
MarcioBelucci <jubeluccips4@gmail.com>
2026-07-02 19:54:39 -0300

    add tire volume calculation script and create volumes file

---
+++ week01/project-tire volume/tire_volume.py
@@ -0,0 +1,7 @@
+import math
+width = float(input("Enter the width of the tire in mm (ex 205): "))
+ratio = float(input("Enter the aspect ratio of the tire (ex 60): "))
+diameter = float(input("Enter the diameter of the wheel in inches (ex 15): "))
+pi = math.pi
+volume = pi * width ** 2 * ratio * (width * ratio + 2_540 * diameter) / 10_000_000_000
+print(f"The approximate volume is {volume:.2f} liters")
