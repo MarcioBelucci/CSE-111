a384347702d066f704ca1a72079b53861c7c297e
MarcioBelucci <jubeluccips4@gmail.com>
2026-07-10 11:40:52 -0300

    Add password validation and improve strength assessment logic

--- week02/passwords.py
+++ week02/passwords.py
@@ -1,4 +1,4 @@
-#I didn't finished the program yet, then I code pass in the function and comment the other code lines.
+#I setted a validation for the user input, it checks if the variable password is empty or only have spaces.
 LOWER=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 UPPER=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 DIGITS=["0","1","2","3","4","5","6","7","8","9"]
@@ -7,14 +7,14 @@ SPECIAL=["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "
 def main():
     password = input("Enter your password(to quit enter 'q' or 'Q'): ")
     while password != "q" and password != "Q":
-        print(f"This is the password: {password}")
+        if password.strip() == "":
+            print("You can't enter an empty password or only spaces. Try again.")
+        else:
+            result_password_strength = password_strength(password, min_length=10, strong_length=16)
+            print(f"The strength of your password is: {result_password_strength}")
         password = input("Enter your password(to quit enter 'q' or 'Q'): ")
-        password_strength(password, 10, 16)
         
-        
-
 def word_complexity(word):
-    """
     count = 0
     if word_has_character(word, LOWER):
         count += 1
@@ -25,31 +25,26 @@ def word_complexity(word):
     if word_has_character(word, SPECIAL):
         count += 1
     return count
-    """
-    pass
 
 def word_has_character(word, character_list):
-    """
     for character in word:
         if character in character_list:
             return True    
     return False
-    """
-    pass
 
 def password_strength(password, min_length, strong_length):
-        """
+
         password_length = len(password)
         complexity_score = word_complexity(password)        
-        word_in_file(password, "toppasswords.txt", True)
-        word_in_file(password, "wordlist.txt", False)
-        if word_in_file == False:
-            print("Password is a dictionaty word and is not secure.")
-            strength = 0
-        elif word_in_file == True:
+        result_wordlist = word_in_file(password, "week02/wordlist.txt", False)
+        result_toppasswords = word_in_file(password, "week02/toppasswords.txt", True)
+        if result_wordlist == True:
+            print("Password is a dictionary word and is not secure.")
+            strength = 0        
+        elif result_toppasswords == True:
             print("Password is a commonly used password and is not secure.")
             strength = 0
-        if password_length < min_length:
+        elif password_length < min_length:
             print("Password is too short and is not secure.")
             strength = 1
         elif password_length > strong_length:
@@ -58,11 +53,8 @@ def password_strength(password, min_length, strong_length):
         else:
             strength = 1 + complexity_score
         return strength
-        """
-        pass
 
 def word_in_file(word, filename,case_sensitive=False):
-    """
     with open(f"{filename}", "r",encoding="utf-8") as file:
         for line in file:
             clean_line = line.strip()
@@ -72,10 +64,7 @@ def word_in_file(word, filename,case_sensitive=False):
             else:
                 if word.lower() == clean_line.lower():
                     return True
-        return False 
-    """
-    pass   
-
-            
+        return False   
+        
 if __name__ == "__main__":
     main()
